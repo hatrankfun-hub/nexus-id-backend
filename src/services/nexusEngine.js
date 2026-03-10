64cbe44fbbe52fa5ce67b11117c36b7eb8f628fe
hatrankfun-hub <hatrankfun@gmail.com>
2026-03-10 13:41:48 +0700

    Update nexusEngine.js

--- src/services/nexusEngine.js
+++ src/services/nexusEngine.js
@@ -1,14 +1,3 @@
-function seededRand(seed) {
-  let h = 0;
-  for (let i = 0; i < seed.length; i++) h = Math.imul(31, h) + seed.charCodeAt(i) | 0;
-  return function() {
-    h = Math.imul(h ^ h >>> 16, 0x45d9f3b);
-    h = Math.imul(h ^ h >>> 16, 0x45d9f3b);
-    h ^= h >>> 16;
-    return (h >>> 0) / 0xffffffff;
-  };
-}
-
 function calcActivityScore({ followersCount, tweetCount, engagementRate, joinedAt }) {
   const followerScore = Math.min(30, (Math.log10(Math.max(followersCount, 1)) / 7) * 30);
   const tweetScore    = Math.min(25, (Math.log10(Math.max(tweetCount, 1)) / 5) * 25);
@@ -20,13 +9,8 @@ function calcActivityScore({ followersCount, tweetCount, engagementRate, joinedA
   return Math.round(Math.min(100, Math.max(0, followerScore + tweetScore + engScore + ageScore)));
 }
 
-function calcIdentity({ username, followersCount, tweetCount, engagementRate, joinedAt }) {
-  const rand          = seededRand(username.toLowerCase());
-  const activityScore = calcActivityScore({ followersCount, tweetCount, engagementRate, joinedAt });
-  const auraIndex     = Math.floor(rand() * 6);
-  const tribeIndex    = Math.floor(rand() * 6);
-  const signalLevel   = Math.max(1, Math.min(6, Math.ceil(activityScore / 17)));
-  return { activityScore, auraIndex, tribeIndex, signalLevel };
+function calcSignalLevel(activityScore) {
+  return Math.max(1, Math.min(6, Math.ceil(activityScore / 17)));
 }
 
-module.exports = { calcIdentity, calcActivityScore };
+module.exports = { calcActivityScore, calcSignalLevel };
